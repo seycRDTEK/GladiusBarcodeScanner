0405d47cf466557b1b6890f7b35aa29df5b1745b
Symphony EYC RDTEK <iosdelivery@iosbuild.local>
2015-07-10 15:14:28 +0200

    APP ID update changes

--- src/android/GladiusBarcodeScanner.java
+++ src/android/GladiusBarcodeScanner.java
@@ -13,7 +13,7 @@ import android.media.MediaPlayer;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-import com.eyc.mobility.GSMActivity;
+import gsm.com.eyc.GSMActivity;
 import com.hsm.barcode.DecodeResult;
 import com.hsm.barcode.Decoder;
 import com.hsm.barcode.DecoderConfigValues;
